5ae0437ae6a4f0c914b1728c0af87e7d4050634a
Liao San-Kai <liaosankai@gmail.com>
2014-05-15 18:20:50 +0800

    netbean auto format

--- classes/Kohana/Email.php
+++ classes/Kohana/Email.php
@@ -1,4 +1,7 @@
-<?php defined('SYSPATH') OR die('No direct access allowed.');
+<?php
+
+defined('SYSPATH') OR die('No direct access allowed.');
+
 /**
  * Email module
  *
@@ -10,359 +13,340 @@
  */
 class Kohana_Email {
 
-	/**
-	 * Default instance name
-	 *
-	 * @var  string
-	 */
-	public static $default = 'default';
+    /**
+     * Default instance name
+     *
+     * @var  string
+     */
+    public static $default = 'default';
 
-	/**
-	 * Database instances
-	 *
-	 * @var  array
-	 */
-	public static $instances = array();
+    /**
+     * Database instances
+     *
+     * @var  array
+     */
+    public static $instances = array();
 
-	/**
-	 * Get a singleton Email instance. If configuration is not specified,
-	 * it will be loaded from the email configuration file using the same
-	 * group as the name.
-	 *
-	 *     // Load the instance with default config
-	 *     $email = Email::instance();
-	 *
-	 *     // Create a custom configured instance
-	 *     $email = Email::instance('custom', $config);
-	 *
-	 * @param   string   $name    instance name
-	 * @param   array    $config  configuration parameters
-	 * @return  Email
-	 */
-	public static function instance($name = NULL, array $config = NULL)
-	{
-		if ($name === NULL)
-		{
-			// Use the default instance name
-			$name = Email::$default;
-		}
+    /**
+     * Get a singleton Email instance. If configuration is not specified,
+     * it will be loaded from the email configuration file using the same
+     * group as the name.
+     *
+     *     // Load the instance with default config
+     *     $email = Email::instance();
+     *
+     *     // Create a custom configured instance
+     *     $email = Email::instance('custom', $config);
+     *
+     * @param   string   $name    instance name
+     * @param   array    $config  configuration parameters
+     * @return  Email
+     */
+    public static function factory($name = NULL, array $config = NULL)
+    {
+        if ($name === NULL) {
+            // Use the default instance name
+            $name = Email::$default;
+        }
 
-		if ( ! isset(Email::$instances[$name]))
-		{
-			if ($config === NULL)
-			{
-				// Load the configuration for this email instance
-				$config = Kohana::$config->load('email')
-					->as_array();
-			}
+        if (!isset(Email::$instances[$name])) {
+            if ($config === NULL) {
+                // Load the configuration for this email instance
+                $config = Kohana::$config->load('email')
+                        ->as_array();
+            }
 
-			if ( ! isset($config[$name]))
-			{
-				throw new Kohana_Exception(':name configuration is not defined',
-					array(':name' => $name));
-			}
+            if (!isset($config[$name])) {
+                throw new Kohana_Exception(':name configuration is not defined', array(':name' => $name));
+            }
 
-			// Store the email instance
-			Email::$instances[$name] = new Email($name, $config[$name]);
-		}
+            // Store the email instance
+            Email::$instances[$name] = new Email($name, $config[$name]);
+        }
 
-		return Email::$instances[$name];
-	}
+        return Email::$instances[$name];
+    }
 
-	/**
-	 * Instance name
-	 *
-	 * @var string
-	 */
-	protected $_instance;
+    /**
+     * Instance name
+     *
+     * @var string
+     */
+    protected $_instance;
 
-	/**
-	 * Raw server connection
-	 */
-	protected $_connection;
+    /**
+     * Raw server connection
+     */
+    protected $_connection;
 
-	/**
-	 * Configuration array
-	 *
-	 * @var array
-	 */
-	protected $_config;
+    /**
+     * Configuration array
+     *
+     * @var array
+     */
+    protected $_config;
 
-	/**
-	 * Stores the email configuration locally and name the instance.
-	 *
-	 * [!!] This method cannot be accessed directly, you must use [Email::instance].
-	 *
-	 * @return  void
-	 */
-	public function __construct($name, array $config)
-	{
-		if ( ! class_exists('Swift_Mailer', FALSE))
-		{
-			// Load SwiftMailer
-			require Kohana::find_file('vendor', 'swiftmailer/lib/swift_required');
-		}
+    /**
+     * Stores the email configuration locally and name the instance.
+     *
+     * [!!] This method cannot be accessed directly, you must use [Email::instance].
+     *
+     * @return  void
+     */
+    public function __construct($name, array $config)
+    {
+        if (!class_exists('Swift_Mailer', FALSE)) {
+            // Load SwiftMailer
+            require Kohana::find_file('vendor', 'swiftmailer/lib/swift_required');
+        }
 
-		// Set the instance name
-		$this->_instance = $name;
+        // Set the instance name
+        $this->_instance = $name;
 
-		// Store the config locally
-		$this->_config = $config;
+        // Store the config locally
+        $this->_config = $config;
 
-		switch ($config['driver'])
-		{
-			case 'smtp':
-				// Set port
-				$port = empty($config['options']['port']) ? 25 : (int) $config['options']['port'];
+        switch ($config['driver']) {
+            case 'smtp':
+                // Set port
+                $port = empty($config['options']['port']) ? 25 : (int) $config['options']['port'];
 
-				// Create SMTP Transport
-				$transport = Swift_SmtpTransport::newInstance($config['options']['hostname'], $port);
+                // Create SMTP Transport
+                $transport = Swift_SmtpTransport::newInstance($config['options']['hostname'], $port);
 
-				if ( ! empty($config['options']['encryption']))
-				{
-					// Set encryption
-					$transport->setEncryption($config['options']['encryption']);
-				}
+                if (!empty($config['options']['encryption'])) {
+                    // Set encryption
+                    $transport->setEncryption($config['options']['encryption']);
+                }
 
-				// Do authentication, if part of the DSN
-				empty($config['options']['username']) OR $transport->setUsername($config['options']['username']);
-				empty($config['options']['password']) OR $transport->setPassword($config['options']['password']);
+                // Do authentication, if part of the DSN
+                empty($config['options']['username']) OR $transport->setUsername($config['options']['username']);
+                empty($config['options']['password']) OR $transport->setPassword($config['options']['password']);
 
-				// Set the timeout to 5 seconds
-				$transport->setTimeout(empty($config['options']['timeout']) ? 5 : (int) $config['options']['timeout']);
-				break;
+                // Set the timeout to 5 seconds
+                $transport->setTimeout(empty($config['options']['timeout']) ? 5 : (int) $config['options']['timeout']);
+                break;
 
-			default:
-				// Use the native connection
-				$transport = Swift_MailTransport::newInstance($config['options']);
-				break;
-		}
+            default:
+                // Use the native connection
+                $transport = Swift_MailTransport::newInstance($config['options']);
+                break;
+        }
 
-		$this->_connection = Swift_Mailer::newInstance($transport);
-	}
+        $this->_connection = Swift_Mailer::newInstance($transport);
+    }
 
-	/**
-	 * Email sender
-	 *
-	 * @var mixed
-	 */
-	protected $_from;
+    /**
+     * Email sender
+     *
+     * @var mixed
+     */
+    protected $_from;
 
-	/**
-	 * Specifies the email sender
-	 *
-	 * @param   string  $email
-	 * @param   string  $name
-	 * @return  Email
-	 */
-	public function from($email, $name = NULL)
-	{
-		$this->_from = (empty($name)) ? $email : array($email => $name);
+    /**
+     * Specifies the email sender
+     *
+     * @param   string  $email
+     * @param   string  $name
+     * @return  Email
+     */
+    public function from($email, $name = NULL)
+    {
+        $this->_from = (empty($name)) ? $email : array($email => $name);
 
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	 * Reply-to data
-	 *
-	 * @var mixed
-	 */
-	protected $_reply_to;
+    /**
+     * Reply-to data
+     *
+     * @var mixed
+     */
+    protected $_reply_to;
 
-	/**
-	 * Specifies the reply-to field
-	 *
-	 * @param   string  $email
-	 * @param   string  $name
-	 * @return  Email
-	 */
-	public function reply_to($email, $name = NULL)
-	{
-		$this->_reply_to = (empty($name)) ? $email : array($email => $name);
+    /**
+     * Specifies the reply-to field
+     *
+     * @param   string  $email
+     * @param   string  $name
+     * @return  Email
+     */
+    public function reply_to($email, $name = NULL)
+    {
+        $this->_reply_to = (empty($name)) ? $email : array($email => $name);
 
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	 * Array of receivers
-	 *
-	 * @var array
-	 */
-	protected $_to = array();
+    /**
+     * Array of receivers
+     *
+     * @var array
+     */
+    protected $_to = array();
 
-	/**
-	 * Adds a recipient
-	 *
-	 * @param   string  $email
-	 * @param   string  $name
-	 * @return  Email
-	 */
-	public function to($email, $name = NULL)
-	{
-		if (empty($name))
-		{
-			$this->_to[] = $email;
-		}
-		else
-		{
-			$this->_to[$email] = $name;
-		}
+    /**
+     * Adds a recipient
+     *
+     * @param   string  $email
+     * @param   string  $name
+     * @return  Email
+     */
+    public function to($email, $name = NULL)
+    {
+        if (empty($name)) {
+            $this->_to[] = $email;
+        } else {
+            $this->_to[$email] = $name;
+        }
 
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	 * Array of receivers
-	 *
-	 * @var array
-	 */
-	protected $_cc = array();
+    /**
+     * Array of receivers
+     *
+     * @var array
+     */
+    protected $_cc = array();
 
-	/**
-	 * Adds a recipient
-	 *
-	 * @param   string  $email
-	 * @param   string  $name
-	 * @return  Email
-	 */
-	public function cc($email, $name = NULL)
-	{
-		if (empty($name))
-		{
-			$this->_cc[] = $email;
-		}
-		else
-		{
-			$this->_cc[$email] = $name;
-		}
+    /**
+     * Adds a recipient
+     *
+     * @param   string  $email
+     * @param   string  $name
+     * @return  Email
+     */
+    public function cc($email, $name = NULL)
+    {
+        if (empty($name)) {
+            $this->_cc[] = $email;
+        } else {
+            $this->_cc[$email] = $name;
+        }
 
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	 * Array of receivers
-	 *
-	 * @var array
-	 */
-	protected $_bcc = array();
+    /**
+     * Array of receivers
+     *
+     * @var array
+     */
+    protected $_bcc = array();
 
-	/**
-	 * Adds a recipient
-	 *
-	 * @param   string  $email
-	 * @param   string  $name
-	 * @return  Email
-	 */
-	public function bcc($email, $name = NULL)
-	{
-		if (empty($name))
-		{
-			$this->_bcc[] = $email;
-		}
-		else
-		{
-			$this->_bcc[$email] = $name;
-		}
+    /**
+     * Adds a recipient
+     *
+     * @param   string  $email
+     * @param   string  $name
+     * @return  Email
+     */
+    public function bcc($email, $name = NULL)
+    {
+        if (empty($name)) {
+            $this->_bcc[] = $email;
+        } else {
+            $this->_bcc[$email] = $name;
+        }
 
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	 * Email subject
-	 *
-	 * @var string
-	 */
-	protected $_subject;
+    /**
+     * Email subject
+     *
+     * @var string
+     */
+    protected $_subject;
 
-	/**
-	 * Specifies the email subject
-	 *
-	 * @param  string $subject
-	 * @return Email
-	 */
-	public function subject($subject)
-	{
-		$this->_subject = $subject;
+    /**
+     * Specifies the email subject
+     *
+     * @param  string $subject
+     * @return Email
+     */
+    public function subject($subject)
+    {
+        $this->_subject = $subject;
 
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	 * Email message content
-	 *
-	 * @var string
-	 */
-	protected $_message;
+    /**
+     * Email message content
+     *
+     * @var string
+     */
+    protected $_message;
 
-	/**
-	 * Message type identifier
-	 *
-	 * @var boolean
-	 */
-	protected $_html;
+    /**
+     * Message type identifier
+     *
+     * @var boolean
+     */
+    protected $_html;
 
-	/**
-	 * Specifies the email message
-	 *
-	 * @param  string  $message
-	 * @param  boolean $html
-	 * @return Email
-	 */
-	public function message($message, $html = FALSE)
-	{
-		$this->_message = $message;
-		$this->_html    = (bool) $html;
+    /**
+     * Specifies the email message
+     *
+     * @param  string  $message
+     * @param  boolean $html
+     * @return Email
+     */
+    public function message($message, $html = FALSE)
+    {
+        $this->_message = $message;
+        $this->_html = (bool) $html;
 
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	 * Sends prepared email
-	 *
-	 * @return void
-	 */
-	public function send()
-	{
-		// Determine the message type
-		$html = ($this->_html) ? 'text/html' : 'text/plain';
+    /**
+     * Sends prepared email
+     *
+     * @return void
+     */
+    public function send()
+    {
+        // Determine the message type
+        $html = ($this->_html) ? 'text/html' : 'text/plain';
 
-		$message = Swift_Message::newInstance($this->_subject, $this->_message, $html, 'utf-8')
-			->setFrom($this->_from);
+        $message = Swift_Message::newInstance($this->_subject, $this->_message, $html, 'utf-8')
+                ->setFrom($this->_from);
 
-		foreach (array('to', 'cc', 'bcc') as $param)
-		{
-			if (sizeof($this->{'_'.$param}) > 0)
-			{
-				$method = 'set'.UTF8::ucfirst($param);
+        foreach (array('to', 'cc', 'bcc') as $param) {
+            if (sizeof($this->{'_' . $param}) > 0) {
+                $method = 'set' . UTF8::ucfirst($param);
 
-				$message->$method($this->{'_'.$param});
-			}
-		}
+                $message->$method($this->{'_' . $param});
+            }
+        }
 
-		if ( ! empty($this->_reply_to))
-		{
-			$message->setReplyTo($this->_reply_to);
-		}
+        if (!empty($this->_reply_to)) {
+            $message->setReplyTo($this->_reply_to);
+        }
 
-		// Send message
-		$this->_connection->send($message);
+        // Send message
+        $this->_connection->send($message);
 
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	 * Reset all recipients (to, cc, bcc rows)
-	 *
-	 * @return Email
-	 */
-	public function reset()
-	{
-		foreach (array('to', 'cc', 'bcc') as $param)
-		{
-			$this->{'_'.$param} = array();
-		}
+    /**
+     * Reset all recipients (to, cc, bcc rows)
+     *
+     * @return Email
+     */
+    public function reset()
+    {
+        foreach (array('to', 'cc', 'bcc') as $param) {
+            $this->{'_' . $param} = array();
+        }
 
-		return $this;
-	}
+        return $this;
+    }
 
-} // End Kohana_Email
+}
+
+// End Kohana_Email
